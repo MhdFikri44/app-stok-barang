7278d4c1afcc784c8ac59f91e65c7ff792ee5bed
Muhammad Fikri <mhdfikr44@gmail.com>
2023-07-22 16:59:55 +0700

    final

--- presentasi/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'components/koneksi.php';
-session_start();
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM tb_data WHERE id=$id ");
-header('location:admin.php');
--- presentasi/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'components/koneksi.php';
-session_start();
-session_destroy();
-header('location:index.php');
